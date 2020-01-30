445522d93e557298e8d9c89794dd4e9bbf1fd571
Peter Bolemant <peter.bolemant@softworx.digital>
2020-01-30 20:29:50 +0100

    general update

--- resources/views/model-viewers/image-viewer/unavailable.blade.php
+++ resources/views/model-viewers/image-viewer/unavailable.blade.php
@@ -1,8 +1,8 @@
+<div class="alert alert-danger">todo model-viewers > image-viewer > unavailable.blade</div>
+
 <div id="{{ $component->getDomId() }}" class="img img-small d-inline-block">
     {{ Html::image('vendor/softworx/rocXolid/images/user-placeholder.svg', $component->getModel()->getTitle()) }}
-todo data.blade
     <div class="btn-group show-up">
         <button class="btn btn-primary" data-ajax-url="{{ $component->getModel()->getControllerRoute('create', [ '_data[model_type]' => $model->getModelName(), '_data[model_id]' => $model->id ]) }}"><i class="fa fa-pencil"></i></button>
     </div>
-
 </div>
