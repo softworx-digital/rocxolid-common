a64c2f679116053fa1b867cab734a57501c676e8
Peter Bolemant <peter.bolemant@softworx.digital>
2020-08-25 12:35:45 +0200

    general update

--- src/Models/Traits/HasAddress.php
+++ src/Models/Traits/HasAddress.php
@@ -2,9 +2,7 @@
 
 namespace Softworx\RocXolid\Common\Models\Traits;
 
-use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
 // common models
 use Softworx\RocXolid\Common\Models\Address;
 
