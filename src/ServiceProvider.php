446faf09c0b6e20586dcb62ced0174bc29fac706
Peter Bolemant <peter@softworx.digital>
2020-11-18 14:49:19 +0000

    general update

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -33,10 +33,10 @@ class ServiceProvider extends RocXolidAbstractServiceProvider
     }
 
     /**
-    * Bootstrap the application services.
-    *
-    * @return void
-    */
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
     public function boot()
     {
         $this
